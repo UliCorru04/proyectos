c888ff0452d797c73792f48779644231eac1f778
UliCorru04 <ulisescolegiotareas@gmail.com>
2024-07-12 15:42:18 -0300

    UPDATE: Button clicker en Hexadecimal y RGB

---
+++ Buttonclicker/BCHexadecimal/main.js
@@ -0,0 +1,37 @@
+let contador = 0; // Su valor cambiará dependiendo del botón presionado.
+let valor = document.getElementById("contador");
+let cambiocolor = document.querySelector(".boton");
+
+
+function Color(){
+    // Esta función crea código hexadecimal.
+    let colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let prefix = "#";
+    for(let i = 0; i < 6; i++){
+        let hex = Math.floor(Math.random() * colors.length);
+        prefix += colors[hex];
+    }
+    return prefix;
+}
+
+function generadorColor(){
+    // Esta función define el valor de la gradiente.
+    let ColorUno = Color();
+    let ColorDos = Color();
+    gradiente='linear-gradient(to left,' +ColorUno+ ',' +ColorDos+')';
+}
+
+document.getElementById('add1').onclick = function Aumento(){
+    // Esta función lleva a cabo el incremento del valor por cada click al botón verde.
+    contador += 1;
+    valor.textContent = contador;
+}
+
+document.getElementById('rem1').onclick = function Disminucion(){
+    // Esta función lleva a cabo la disminución del valor por cada click al botón rojo. Ademas
+    // del color de fondo aleatorio.
+    contador -= 1;
+    valor.textContent = contador;
+    generadorColor();
+    cambiocolor.style.backgroundImage= gradiente;
+}
