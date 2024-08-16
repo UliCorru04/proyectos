22c8b956ae621356befd58abb5bf481c3cb8bc6a
UliCorru04 <ulisescolegiotareas@gmail.com>
2024-08-16 12:05:04 -0300

    Agregado multiplicador

--- Buttonclicker/CountClicker/main.js
+++ Buttonclicker/CountClicker/main.js
@@ -1,21 +1,25 @@
 let sumar = document.querySelector('#sumar');
-let n = 0;
 let mejorar = document.querySelector('#mejorar');
+let limite = 50;
+let n = 0;
+let sumador = 1;
 mejorar.disabled = true;
 
-function sumaMejora(){
+function sumaMejorar(){
     if (event.target.id == 'sumar'){
-        let h1 = Document.querySelector('#contador');
-        n = h1.textContent;
-        n++;
+        let h1 = document.querySelector('#contador');
+        n = +h1.textContent;
+        n += sumador;
         h1.textContent = n;
     }
-    if (n == 50){
+    if (n == limite){
         mejorar.disabled = false;
         sumar.disabled = true;
-        if (event.target.id == 'mejora'){
+        if (event.target.id == 'mejorar'){
             mejorar.disabled = true;
             sumar.disabled = false;
+            sumador += 1;
+            sumador <= 3 ? limite *= 3 : limite *= 2.1;
         }
     }
 }
