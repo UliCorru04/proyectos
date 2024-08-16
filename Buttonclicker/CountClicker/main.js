00b1960139e0287329e7d0ce08ec1ee5de222d9a
UliCorru04 <ulisescolegiotareas@gmail.com>
2024-08-16 17:50:28 -0300

    Customización y diamantes (ALPHA)

--- Buttonclicker/CountClicker/main.js
+++ Buttonclicker/CountClicker/main.js
@@ -1,16 +1,25 @@
 let sumar = document.querySelector('#sumar');
 let mejorar = document.querySelector('#mejorar');
+let h1 = document.querySelector('#contador');
 let limite = 50;
 let n = 0;
 let sumador = 1;
+let monedas = 0;
+let clicks = 0;
 mejorar.disabled = true;
 
 function sumaMejorar(){
     if (event.target.id == 'sumar'){
-        let h1 = document.querySelector('#contador');
         n = +h1.textContent;
         n += sumador;
         h1.textContent = n;
+        clicks++;
+        if (clicks % 75 == 0){
+            let moneda = document.querySelector('#monedas');
+            monedas = +moneda.textContent;
+            monedas++;
+            moneda.textContent = monedas, "💎";
+        }
     }
     if (n == limite){
         mejorar.disabled = false;
@@ -19,7 +28,57 @@ function sumaMejorar(){
             mejorar.disabled = true;
             sumar.disabled = false;
             sumador += 1;
-            sumador <= 3 ? limite *= 3 : limite *= 2.1;
+                limite *= 3;
         }
+    }
+}
+
+function cambioColor(){
+    if (event.target.id == 'rojo'){
+        let caja = document.querySelector('#caja');
+        let sumar = document.querySelector('#sumar');
+        let mejorar = document.querySelector('#mejorar:enabled');
+        caja.style.border = 'solid 1px rgb(255, 0, 0)';
+        h1.style.color = 'rgb(255, 0, 0)';
+        sumar.style.color = 'rgb(255, 0, 0)';
+        sumar.style.border = 'solid 2px rgb(255, 0, 0)';
+        mejorar.style.color = 'rgb(255, 0, 0)';
+        mejorar.style.border = 'solid 2px rgb(255, 0, 0)';
+    }
+
+    if (event.target.id == 'azul'){
+        let caja = document.querySelector('#caja');
+        let sumar = document.querySelector('#sumar');
+        let mejorar = document.querySelector('#mejorar:enabled');
+        caja.style.border = 'solid 1px rgb(0, 0, 255)';
+        h1.style.color = 'rgb(0, 0, 255)';
+        sumar.style.color = 'rgb(0, 0, 255)';
+        sumar.style.border = 'solid 2px rgb(0, 0, 255)';
+        mejorar.style.color = 'rgb(0, 0, 255)';
+        mejorar.style.border = 'solid 2px rgb(0, 0, 255)';
+    }
+
+    if (event.target.id == 'verde'){
+        let caja = document.querySelector('#caja');
+        let sumar = document.querySelector('#sumar');
+        let mejorar = document.querySelector('#mejorar:enabled');
+        caja.style.border = 'solid 1px rgb(0, 255, 0)';
+        h1.style.color = 'rgb(0, 255, 0)';
+        sumar.style.color = 'rgb(0, 255, 0)';
+        sumar.style.border = 'solid 2px rgb(0, 255, 0)';
+        mejorar.style.color = 'rgb(0, 255, 0)';
+        mejorar.style.border = 'solid 2px rgb(0, 255, 0)';
+    }
+
+    if (event.target.id == 'violeta'){
+        let caja = document.querySelector('#caja');
+        let sumar = document.querySelector('#sumar');
+        let mejorar = document.querySelector('#mejorar:enabled');
+        caja.style.border = 'solid 1px rgb(255, 0, 255)';
+        h1.style.color = 'rgb(255, 0, 255)';
+        sumar.style.color = 'rgb(255, 0, 255)';
+        sumar.style.border = 'solid 2px rgb(255, 0, 255)';
+        mejorar.style.color = 'rgb(255, 0, 255)';
+        mejorar.style.border = 'solid 2px rgb(255, 0, 255)';
     }
 }
